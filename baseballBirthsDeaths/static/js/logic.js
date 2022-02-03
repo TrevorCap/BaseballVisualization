1ea86bc241eeaa08964d173507d5c58312f114a6
Trevor Cappa <47541119+TrevorCap@users.noreply.github.com>
2022-02-02 17:27:19 -0800

    Update logic.js

--- baseballBirthsDeaths/static/js/logic.js
+++ baseballBirthsDeaths/static/js/logic.js
@@ -28,7 +28,7 @@ var myMap = L.map("map", {
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
  
